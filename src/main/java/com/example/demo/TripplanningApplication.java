8024276eda2516b8b5525bf061b77049a378c7e4
djqui <joannadenise.quinto.mnl@eac.edu.ph>
2025-05-26 07:07:54 +0800

    Add WAR packaging and configure Spring Boot application for servlet support

--- src/main/java/com/example/demo/TripplanningApplication.java
+++ src/main/java/com/example/demo/TripplanningApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class TripplanningApplication {
+public class TripplanningApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TripplanningApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripplanningApplication.class, args);
